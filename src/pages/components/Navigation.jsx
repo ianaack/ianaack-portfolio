867c8b9f22291381b19c8d2b9203fca04c269d7d
ianaack <ianaack@gmail.com>
2023-04-03 16:21:37 -0400

    Styling: centered nav elements

--- src/pages/components/Navigation.jsx
+++ src/pages/components/Navigation.jsx
@@ -4,24 +4,26 @@ import NavItem from "./sub-components/NavItem";
 
 const Navigation = () => {
 	return (
-		<div className="mx-3">
-			<nav className="navbar navbar-expand-lg">
-				<a className="navbar-brand" href="/">
-					<img src={logo} alt="Ian Ackerman" style={{ height: "50px" }} />
-				</a>
-				<button
-					className="navbar-toggler btn btn-secondary"
-					type="button"
-					data-bs-toggle="collapse"
-					data-bs-target="#navbarNav"
-					aria-controls="navbarNav"
-					aria-expanded="false"
-					aria-label="Toggle Navigation"
-				>
-					<span className="navbar-toggler-icon"></span>
-				</button>
-				<NavItem />
-			</nav>
+		<div className="container">
+			<div className="mx-3">
+				<nav className="navbar navbar-expand-lg">
+					<a className="navbar-brand" href="/">
+						<img src={logo} alt="Ian Ackerman" style={{ height: "50px" }} />
+					</a>
+					<button
+						className="navbar-toggler btn btn-secondary"
+						type="button"
+						data-bs-toggle="collapse"
+						data-bs-target="#navbarNav"
+						aria-controls="navbarNav"
+						aria-expanded="false"
+						aria-label="Toggle Navigation"
+					>
+						<span className="navbar-toggler-icon"></span>
+					</button>
+					<NavItem />
+				</nav>
+			</div>
 		</div>
 	);
 };
