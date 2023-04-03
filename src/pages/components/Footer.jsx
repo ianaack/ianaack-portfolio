361141c2aa013eb77b69afcfbb96833930467be9
ianaack <ianaack@gmail.com>
2023-04-03 16:16:00 -0400

    Updated: styling and footer element

--- src/pages/components/Footer.jsx
+++ src/pages/components/Footer.jsx
@@ -1,39 +1,47 @@
 import React from "react";
 
 function Footer() {
+	const year = new Date().getFullYear();
+
 	return (
-			<div className="social-media-list">
-				<li className="mx-3">
-					<a
-						target="_blank"
-						rel="noreferrer"
-						href="https://github.com/ianaack"
-						className="social-icon"
-					>
-						<span className="bi bi-github"></span>
-					</a>
-				</li>
-				<li className="mx-3">
-					<a
-						target="_blank"
-						rel="noreferrer"
-						href="https://linkedin.com/in/ianaack"
-						className="social-icon"
-					>
-						<span className="bi bi-linkedin"></span>
-					</a>
-				</li>
-				<li className="mx-3">
-					<a
-						target="_blank"
-						rel="noreferrer"
-						href="https://twitter.com/ianaack"
-						className="social-icon"
-					>
-						<span className="bi bi-twitter"></span>
-					</a>
-				</li>
+		<footer className="footer py-4">
+			<div className="container">
+				<div className="row align-items-center">
+					<h5 className="col-lg-6">
+						Copyright &copy; Ian Ackerman {year}
+					</h5>
+					<div className="col-lg-4 my-3 my-lg-0">
+						<a
+							className="btn btn-dark btn-social mx-2"
+							target="_blank"
+							rel="noreferrer"
+							href="https://twitter.com/ianaack"
+							aria-label="Twitter"
+						>
+							<i className="bi bi-twitter"></i>
+						</a>
+						<a
+							className="btn btn-dark btn-social mx-2"
+							target="_blank"
+							rel="noreferrer"
+							href="https://github.com/ianaack"
+							aria-label="Github"
+						>
+							<i className="bi bi-github"></i>
+						</a>
+						<a
+							className="btn btn-dark btn-social mx-2"
+							target="_blank"
+							rel="noreferrer"
+							href="https://linkedin.com/in/ianaack"
+							aria-label="LinkedIn"
+						>
+							<i className="bi bi-linkedin"></i>
+						</a>
+					</div>
+				</div>
 			</div>
+		</footer>
 	);
 }
 
