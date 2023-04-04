ab8c521350932079c1386d9b7d9201dd94a4fe40
ianaack <ianaack@gmail.com>
2023-04-03 20:17:29 -0400

    Updated: project cards

--- src/pages/components/sub-components/Project.jsx
+++ src/pages/components/sub-components/Project.jsx
@@ -5,55 +5,58 @@ import projectImage3 from "../../../assets/projectImages/generatedMarkdown.png";
 import projectImage4 from "../../../assets/projectImages/food-finder.png";
 import projectImage5 from "../../../assets/projectImages/pet-stuff-plus.png";
 import projectImage6 from "../../../assets/projectImages/reimagined-systems.png";
+import projectImage7 from "../../../assets/projectImages/t-b_auto.png";
 
 const projects = [
 	{
+		name: "T&B Automotive",
+		image: projectImage7,
+		description: "",
+		deployedUrl: "https://tandbauto.ca",
+		githubUrl: "https://github.com/ianaack/tbauto",
+	},
+	{
 		name: "Pet Stuff Plus!",
 		image: projectImage5,
+		description: "",
 		deployedUrl: "https://petstuffplus.herokuapp.com/",
 		githubUrl: "https://github.com/project0301/paws",
-		technologies:
-			"React, Chakra UI, Apollo Client, GraphQL, JSON Web Token, JWT-decode, MongoDB, Mongoose, Express, Node.js, bcrypt, JSX/Javascript, HTML, CSS",
 	},
 	{
 		name: "Food Finder",
 		image: projectImage4,
+		description: "",
 		deployedUrl: "https://ianaack.github.io/food-finder/",
 		githubUrl: "https://github.com/ianaack/food-finder",
-		technologies:
-			"HTML, Tailwind CSS, Javascript, Yelp Fusion API, Google Geocoding API, Google Directions API",
 	},
 
 	{
 		name: "Reimagined Systems",
 		image: projectImage6,
+		description: "",
 		deployedUrl: "https://reimagined-system.herokuapp.com/",
 		githubUrl: "https://github.com/ianaack/reimagined-system",
-		technologies:
-			"HTML, Tailwind CSS, Javascript, Express, Node.js, Handlebars.js, MySQL, Sequelize, bcrypt, chart.js",
 	},
 	{
 		name: "Budget Tracker",
 		image: projectImage1,
+		description: "",
 		deployedUrl: "https://ianaack-budget-tracker.herokuapp.com/",
 		githubUrl: "https://github.com/ianaack/budget-tracker",
-		technologies:
-			"MongoDB, Express, Mongoose, Node.js, PWA, HTML, CSS, JavaScript",
 	},
 	{
 		name: "Dev Tech Blog",
 		image: projectImage2,
+		description: "",
 		deployedUrl: "https://dev-tech-blog-ianaack.herokuapp.com/",
 		githubUrl: "https://github.com/ianaack/dev-tech-blog",
-		technologies:
-			"Node.js, MySQL, Express, Sequelize, dotenv, bcrypt, Handlebars.js",
 	},
 	{
-		name: "Professional README Generator",
+		name: "README Generator",
 		image: projectImage3,
+		description: "",
 		deployedUrl: "https://github.com/ianaack/pro-readme-generator",
 		githubUrl: "https://github.com/ianaack/pro-readme-generator",
-		technologies: "Node.js, File System, Inquirer, JavaScript ",
 	},
 ];
 
@@ -61,24 +64,24 @@ function Project() {
 	const projectCards = [];
 
 	for (let i = 0; i < projects.length; i++) {
-		const { name, image, deployedUrl, githubUrl, technologies } = projects[i];
+		const { name, image, deployedUrl, githubUrl } = projects[i];
 
 		const card = (
-			<div className="col-lg-4 col-sm-6 mb-4" key={name}>
-				<div className="project-item">
-					<img className="project-image img-fluid" src={image} alt={name} />
-					<div className="project-caption">
-						<div className="project-caption-heading">{name}</div>
-						<div className="project-caption-subheading">
-							Built with: {technologies}!
-							<br />
-							<a href={deployedUrl} className="project-caption-link h5">
-								View the Deployed Application
-							</a>
-							<br />
-							<a href={githubUrl} className="project-caption-link h6">
-								View the Github Repo!
-							</a>
+			<div className="col-lg-3 col-md-6 col-sm-12 my-2" key={name}>
+				<div className="card" id="project">
+					<img className="card-img-top" src={image} alt={name} />
+					<div className="card-body">
+						<h5 className="card-title fw-bold text-center">{name}</h5>
+						<div className="text-center">
+							<div className="btn-group">
+								<a href={deployedUrl} className="btn btn-primary">
+									Deployed
+								</a>
+								<br />
+								<a href={githubUrl} className="btn btn-secondary">
+									Github
+								</a>
+							</div>
 						</div>
 					</div>
 				</div>
